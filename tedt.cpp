cba3eb6534ed7e4062f2196a461974b98bb7fd70
ShoHuu <83387636+BusinessBo@users.noreply.github.com>
2024-08-24 15:24:26 +0800

    Update tedt.cpp

--- tedt.cpp
+++ tedt.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+
 using namespace std;
-int main()
-{
-cout << "Hello World";
-return 0;
+
+int main() {
+    int Sales = 95000;
+    double State_Tax  = 0.04;
+    double County_Tax = 0.02;
+    double x = Sales * State_Tax;
+    double y = Sales * County_Tax;
+    int z = Sales - (x+y);
+    cout << "Sales = $" << Sales << endl
+         << "State Tax = $" << x << endl
+         << "County Tax = $" << y << endl
+         << "Money Left = $" << z;
+    return 0;
 }
