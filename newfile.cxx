ea47e35776dd6ce21a3e83be33b18757bdb8070c
ShoHuu <83387636+BusinessBo@users.noreply.github.com>
2026-03-23 20:50:52 +0800

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int snumber;
+	string operation;
+	int fnumber;
+	
+	cout << "Please enter the first number: ";
+	cin >> fnumber;
+	
+	cout << "Please choose the operation" << endl << " +  -  /  *" << endl;
+	cin >> operation;
+	
+	cout << "Please enter the second number: ";
+	cin >> snumber;
+	
+	if (operation == "+") {
+		cout << "Answer: " <<fnumber + snumber << endl;
+	}
+	
+	if (operation == "-") {
+		cout << "Answer: " << fnumber - snumber << endl;
+	}
+	
+	if (operation == "/") {
+		cout << "Answer: " << fnumber / snumber << endl;
+	}
+	
+	if (operation == "*") {
+		cout << "Answer: " << fnumber * snumber << endl;
+	}
+	return 0;
+	
+}
+	
